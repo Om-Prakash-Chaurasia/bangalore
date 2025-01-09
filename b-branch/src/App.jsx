fcd61eb43d611a9a1d7a09bdda6adf82c4c2b0d8
OmPrakashChaurasia <omchaurasia2024@gmail.com>
2025-01-09 12:29:29 +0530

    first commit

---
+++ b-branch/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import "./App.css";
+import Greetings from "./Greetings";
+
+function App() {
+  return (
+    <div>
+      App
+      <Greetings />
+    </div>
+  );
+}
+
+export default App;
---
+++ b-branch/src/Greetings.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greetings() {
+  return <div>Hello, Everyone</div>;
+}
+
+export default Greetings;
